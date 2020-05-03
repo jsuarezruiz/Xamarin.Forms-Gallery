6d0d10003fa1c53016a80d1f0d705a28c1f6dcf6
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2020-05-03 13:52:28 +0200

    Fixed custom searchbar appareance in iOS

---
+++ src/Xamarin.Forms.Gallery.iOS/Renderers/CustomEntryRenderer.cs
@@ -0,0 +1,18 @@
+﻿using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Gallery.iOS.Renderers;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(Entry), typeof(CustomEntryRenderer))]
+namespace Xamarin.Forms.Gallery.iOS.Renderers
+{
+    public class CustomEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            Control.BorderStyle = UITextBorderStyle.None;
+        }
+    }
+}
